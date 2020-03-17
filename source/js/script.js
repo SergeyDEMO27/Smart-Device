d740b99194b0df05bfb5984f1e6f6b27f5f1609c
edji777 <49676371+edji777@users.noreply.github.com>
2020-03-17 19:54:44 +0300

    добавляет попап

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,25 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var openPopup = document.querySelector('.contacts-list__link');
+var popup = document.querySelector(".feedback--popup");
+var mainPage = document.querySelector(".index-page");
 
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+openPopup.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  console.log(popup);
+  popup.classList.add("show");
+  mainPage.classList.add("overlay");
 });
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
+//
+// pageHeader.classList.remove('page-header--nojs');
+//
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
