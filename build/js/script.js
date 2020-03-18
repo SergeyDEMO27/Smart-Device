725aeb99fb681aeee5c1c32c496a78b44cf9282d
edji777 <49676371+edji777@users.noreply.github.com>
2020-03-18 11:45:13 +0300

    вносит исправления

--- build/js/script.js
+++ build/js/script.js
@@ -1,25 +1,29 @@
 'use strict';
 var openPopup = document.querySelector('.contacts-list__link');
 var popup = document.querySelector(".feedback--popup");
+var closePopup = document.querySelector(".feedback__close");
 var mainPage = document.querySelector(".index-page");
+var popupName = document.querySelector(".feedback__name");
 
 openPopup.addEventListener('click', function (evt) {
   evt.preventDefault();
-  console.log(popup);
   popup.classList.add("show");
   mainPage.classList.add("overlay");
+  popupName.focus();
 });
-// var pageHeader = document.querySelector('.page-header');
-// var headerToggle = document.querySelector('.page-header__toggle');
-//
-// pageHeader.classList.remove('page-header--nojs');
-//
-// headerToggle.addEventListener('click', function () {
-//   if (pageHeader.classList.contains('page-header--closed')) {
-//     pageHeader.classList.remove('page-header--closed');
-//     pageHeader.classList.add('page-header--opened');
-//   } else {
-//     pageHeader.classList.add('page-header--closed');
-//     pageHeader.classList.remove('page-header--opened');
-//   }
-// });
+
+closePopup.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("show");
+  mainPage.classList.remove("overlay");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+  if (popup.classList.contains("show")) {
+    popup.classList.remove("show");
+    mainPage.classList.remove("overlay");
+  }
+}
+});
